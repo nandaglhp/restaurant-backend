4f01c7b5d91579e5155d108a844875677d8b5c4b
= <=>
2024-06-13 03:48:11 +0700

    fix(server): fix typo

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,21 @@
-import cors from 'cors';
-import restaurants from './restaurant';
-import server from 'json-server';
-import * as dotenv as dotenv;
-
+import cors from "cors";
+import restaurants from "./restaurant";
+import server from "json-server";
+import * as dotenv from "dotenv";
 
 dotenv.config();
 const app = server.create();
 const port = process.env.PORT || 3000;
 const router = server.router(restaurants);
 
-app.use(cors({
-	origin: process.env.NODE_FRONTEND_URL,
-	credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.NODE_FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(router);
 
 app.listen(port, () => {
-	console.log('Server running on port ' + port);
+  console.log("Server running on port " + port);
 });
